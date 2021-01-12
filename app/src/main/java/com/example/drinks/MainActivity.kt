7e6cc67ee3a4f7b8d71124696151d0c3112b1084
Andrey <a.ponomarev.e@gmail.com>
2021-01-12 19:38:52 +0200

    Create base layouts

--- app/src/main/java/com/example/drinks/MainActivity.kt
+++ app/src/main/java/com/example/drinks/MainActivity.kt
@@ -15,10 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -32,8 +29,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_filters -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun clickFilter(item: MenuItem) {}
 }
